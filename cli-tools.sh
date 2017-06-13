27b9416f46d6535267041680b11c6ef181f11e34
Pedro Cavalheiro <pedrohc@gmail.com>
2017-06-13 13:26:27 -0300

    Adds tmux from source + some libs + spotify client

--- cli-tools.sh
+++ cli-tools.sh
@@ -4,6 +4,8 @@
 # https://stedolan.github.io/jq/
 # https://github.com/jkbrzt/httpie
 
+#/etc/default/keyboard -> XKBOPTIONS="ctrl:nocaps"
+
 function install {
   local package=$1
 
@@ -18,15 +20,14 @@ function install {
 
 echo "deb-src http://ppa.launchpad.net/nathan-renniewaldock/flux/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/flux.list
 echo "deb http://ppa.launchpad.net/nathan-renniewaldock/flux/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/flux.list
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 sudo apt-get update
 sudo apt-get upgrade -y
 #sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 
-#/etc/default/keyboard -> XKBOPTIONS="ctrl:nocaps"
-
 install software-properties-common
-install tmux
 install git
 install htop
 install atop
@@ -46,12 +47,15 @@ install pepperflashplugin-nonfree
 install lm-sensors
 install guake
 install gconf-service
+install libyaml-dev
 install libgconf-2-4
 install libnspr4
 install libnss3
 install libpango1.0-0
 install libappindicator1
 install libcurl3
+install libpq-dev
+install libevent-dev
 install playonlinux
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
@@ -59,6 +63,7 @@ rm google-chrome-stable_current_amd64.deb
 install gtk-theme-switch
 install exuberant-ctags
 install tree
+install meld
 install python3-dev
 install python3-pip
 sudo pip3 install thefuck
@@ -67,6 +72,7 @@ install libx11-dev
 install libgl1-mesa-dev
 install libjpeg-dev
 install libpng12-dev
+install spotify-client
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
@@ -75,6 +81,10 @@ sudo apt-get install fluxgui
 git clone https://github.com/glmark2/glmark2.git
 cd glmark2 && ./waf configure --with-flavors=x11-gl && ./waf build -j 4 && sudo ./waf install && sudo strip -s /usr/local/bin/glmark2
 rm -rf glmark2
-\curl -sSL https://get.rvm.io | bash
+\curl -L https://get.rvm.io | bash -s stable --ruby
 source /home/$USER/.rvm/scripts/rvm
-rvm install ruby-2.3.1
+wget https://github.com/tmux/tmux/releases/download/2.4/tmux-2.4.tar.gz
+tar -xzf tmux-2.4.tar.gz
+cd tmux-2.4 && ./configure && make && sudo make install
+rm -rf tmux-2.4*
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
