ae922418a0c9af783c3e79df1414172be84605b7
Pedro Cavalheiro <pedrohc@gmail.com>
2018-03-16 19:31:27 +0000

    Updates some apps versions

--- cli-tools.sh
+++ cli-tools.sh
@@ -20,8 +20,10 @@ function install {
 
 echo "deb-src http://ppa.launchpad.net/nathan-renniewaldock/flux/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/flux.list
 echo "deb http://ppa.launchpad.net/nathan-renniewaldock/flux/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/flux.list
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get upgrade -y
 #sudo apt-get dist-upgrade -y
@@ -75,16 +77,16 @@ install libpng12-dev
 install spotify-client
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
-sudo curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install fluxgui
-git clone https://github.com/glmark2/glmark2.git
-cd glmark2 && ./waf configure --with-flavors=x11-gl && ./waf build -j 4 && sudo ./waf install && sudo strip -s /usr/local/bin/glmark2
-rm -rf glmark2
+#git clone https://github.com/glmark2/glmark2.git
+#cd glmark2 && ./waf configure --with-flavors=x11-gl && ./waf build -j 4 && sudo ./waf install && sudo strip -s /usr/local/bin/glmark2
+#rm -rf glmark2
 \curl -L https://get.rvm.io | bash -s stable --ruby
 source /home/$USER/.rvm/scripts/rvm
-wget https://github.com/tmux/tmux/releases/download/2.4/tmux-2.4.tar.gz
-tar -xzf tmux-2.4.tar.gz
-cd tmux-2.4 && ./configure && make && sudo make install
-rm -rf tmux-2.4*
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+tar -xzf tmux-2.6.tar.gz
+cd tmux-2.6 && ./configure && make && sudo make install
+rm -rf tmux-2.6*
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
