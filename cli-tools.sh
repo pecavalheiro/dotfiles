9e496114cf3e30d9d867a98269e26b4101fd4417
Pedro Cavalheiro <pedrohc@gmail.com>
2018-05-05 19:14:07 -0300

    Add redshift to cli-tools

--- cli-tools.sh
+++ cli-tools.sh
@@ -41,6 +41,7 @@ install curl
 install ack-grep
 install silversearcher-ag
 install powertop
+install cups
 install libreoffice
 install vlc
 install build-essential
@@ -90,7 +91,8 @@ install python-gconf
 install python-appindicator
 install libjpeg-dev
 install libpng-dev
-git clone "https://github.com/xflux-gui/fluxgui.git" && cd fluxgui && python download-xflux.py && sudo python setup.py install && cd .. && rm -rf fluxgui
+install intltool
+wget https://github.com/jonls/redshift/releases/download/v1.11/redshift-1.11.tar.xz && tar -xf redshift-1.11.tar.xz && cd redshift-1.11 && ./configure && sudo make && sudo make install && cd .. && rm -rf redshift-1.11* && cp redshift.conf ~/.config/
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc && mkdir -p ~/plugins && git clone https://github.com/rbenv/ruby-build.git ~/plugins/ruby-build
 wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz && tar -xzf tmux-2.6.tar.gz && cd tmux-2.6 && ./configure && make && sudo make install && cd .. && rm -rf tmux-2.6*
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
