a300e91d551df5e6bca3fd9edab2b8566dd56812
Pedro Cavalheiro <pedrohc@gmail.com>
2020-10-07 14:28:48 +0200

    Update linux initial install script

--- cli-tools.sh
+++ cli-tools.sh
@@ -35,9 +35,12 @@ function remove {
 # adduser <username> sudo
 
 install dirmngr
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-echo deb https://dl.winehq.org/wine-builds/debian/ stretch main | sudo tee /etc/apt/sources.list.d/wine.list
+echo deb https://dl.winehq.org/wine-builds/debian/ buster main | sudo tee /etc/apt/sources.list.d/wine.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 # Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo add-apt-repository ppa:jonathonf/vim
 sudo dpkg --add-architecture i386
 sudo wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
@@ -83,16 +86,12 @@ install libreadline-dev
 install intltool
 sudo apt-get install -y --install-recommends winehq-stable
 install playonlinux
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-install gtk-theme-switch
 install exuberant-ctags
 install tree
 install meld
 install python3-dev
 install python3-pip
-install python-pygments
+install python3-pygments
 sudo pip3 install thefuck
 install pkg-config
 install libx11-dev
@@ -111,6 +110,9 @@ install openvpn
 install python3-setuptools
 install python-expect
 install intltool
+install docker-ce
+install docker-ce-cli
+install containerd.io
 
 # Openpyn
 sudo python3 -m pip install openpyn --upgrade
@@ -118,25 +120,15 @@ sudo python3 -m pip install openpyn --upgrade
 # Other
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Docker 
-curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker $USER
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
-
 # Rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc && mkdir -p ~/.rbenv/plugins && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.5.1 && rbenv global 2.5.1
 
 # Tmux
-wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz && tar -xzf tmux-2.6.tar.gz && cd tmux-2.6 && ./configure && make && sudo make install && cd .. && rm -rf tmux-2.6*
+wget https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b.tar.gz && tar -xzf tmux-3.1b.tar.gz && cd tmux-3.1b && ./configure && make && sudo make install && cd .. && rm -rf tmux-3.1*
 
-# Franz
-wget https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.17/franz_5.0.0-beta.17_amd64.deb && sudo dpkg -i franz_5.0.0-beta.17_amd64.deb && rm franz_5.0.0-beta.17_amd64.deb
 
 # Nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && nvm install node && npm install -g diff-so-fancy
-
-# Redshift
-wget https://github.com/jonls/redshift/releases/download/v1.11/redshift-1.11.tar.xz && tar -xf redshift-1.11.tar.xz && cd redshift-1.11 && ./configure && sudo make && sudo make install && cd .. && rm -rf redshift-1.11* && cp redshift.conf ~/.config/
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.36.0/install.sh | bash && nvm install node && npm install -g diff-so-fancy
 
 # Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -145,4 +137,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 remove nano
 
 # Topicons
-google-chrome https://extensions.gnome.org/extension/495/topicons/
+# google-chrome https://extensions.gnome.org/extension/495/topicons/
