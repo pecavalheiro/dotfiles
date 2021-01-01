842e6cbdef812b80434df1e77974125b94cf9cb1
Pedro Cavalheiro <pedrohc@gmail.com>
2021-01-01 18:10:45 +0100

    Add script to fix lenovo webcam temperature

---
+++ lenovo-webcam-color-fix.sh
@@ -0,0 +1,6 @@
+v4l2-ctl --set-ctrl=white_balance_temperature_auto=0
+v4l2-ctl --set-ctrl=white_balance_temperature=4200
+v4l2-ctl --set-ctrl=saturation=70
+v4l2-ctl --set-ctrl=brightness=150
+v4l2-ctl --set-ctrl=contrast=50
+v4l2-ctl --set-ctrl=sharpness=5
