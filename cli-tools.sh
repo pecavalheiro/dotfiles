7588fe3b7bb44d89b008e1595eabbfd2502b9b70
Pedro Cavalheiro <pedrohc@gmail.com>
2021-01-13 17:20:38 +0100

    Update cli-tools for Debian testing (Bullseye)

--- cli-tools.sh
+++ cli-tools.sh
@@ -30,105 +30,122 @@ function remove {
   fi
 }
 
-#install sudo
-#exit 0
-# adduser <username> sudo
+# Apple magic keyboard 
+# default with Fn keys
+sudo echo 2 >> /sys/module/hid_apple/parameters/fnmode
+# swap option with command key
+sudo echo 1 >> /sys/module/hid_apple/parameters/swap_opt_cmd
+# /Apple magic keyboard 
 
-install dirmngr
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-echo deb https://dl.winehq.org/wine-builds/debian/ buster main | sudo tee /etc/apt/sources.list.d/wine.list
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88 # Docker
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo add-apt-repository ppa:jonathonf/vim
+# Postgres
+sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# /Postgres
+
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+# /Spotify
+
+# Wine/PlayOnLinux
 sudo dpkg --add-architecture i386
 sudo wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-sudo wget -q "https://dl.winehq.org/wine-builds/Release.key" -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
+echo deb http://deb.playonlinux.com/ stretch main | sudo tee /etc/apt/sources.list.d/playonlinux.list
+# /Wine/PlayOnLinux
 
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 # Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
+# /Docker
+
+# 1Password
+sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://downloads.1password.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
+# /1Password
+
+# OpenRazer
+echo 'deb http://download.opensuse.org/repositories/hardware:/razer/Debian_Testing/ /' | sudo tee /etc/apt/sources.list.d/hardware:razer.list
+curl -fsSL https://download.opensuse.org/repositories/hardware:razer/Debian_Testing/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/hardware_razer.gpg > /dev/null
+# /OpenRazer
+
+# make sources changes effective
+sudo apt-get update
+
+# docker
+install apt-transport-https
+install ca-certificates
+install curl
+install gnupg-agent
 install software-properties-common
+install docker-ce
+install docker-ce-cli
+install containerd.io
+# /docker
+
+# Wine/PlayOnLinux
+install wine
+install wine32
+install wine64
+install libwine
+install libwine:i386
+install fonts-wine
+install playonlinux
+# /Wine
+
+# Postgres
+install libpq-dev
+# /Postgres
+
+install spotify-client
 install git
 install htop
 install iotop
-install atop
 install vim
 install tmux
-install xclip
 install zsh
-install curl
-install ack-grep
 install silversearcher-ag
 install powertop
-install cups
 install libreoffice
 install vlc
-install build-essential
 install tar
-install firmware-linux
-install intel-microcode
-install lm-sensors
 install guake
-install gconf-service
-install libyaml-dev
-install libgconf-2-4
-install libnspr4
-install libnss3
-install libpango1.0-0
-install libappindicator1
-install libcurl3
-install libpq-dev
-install libevent-dev
-install libssl-dev
-install libreadline-dev
-install intltool
-sudo apt-get install -y --install-recommends winehq-stable
-install playonlinux
 install exuberant-ctags
 install tree
 install meld
 install python3-dev
 install python3-pip
 install python3-pygments
-sudo pip3 install thefuck
-install pkg-config
-install libx11-dev
-install libgl1-mesa-dev
-install libjpeg-dev
-install libpng-dev
-install spotify-client
-install ncurses-dev
 install fonts-powerline
 install fonts-roboto
-install bash-completion
-install python-pexpect
-install python-gconf
-install python-appindicator
-install openvpn
-install python3-setuptools
-install python-expect
-install intltool
-install docker-ce
-install docker-ce-cli
-install containerd.io
+install 1password
+install direnv
+install snapd
+install yarn
+install postgresql-9.6
+install openrazer-meta
 
-# Openpyn
-sudo python3 -m pip install openpyn --upgrade
+# Direnv
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
-# Other
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Thefuck
+sudo pip3 install thefuck
+
+# Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc && mkdir -p ~/.rbenv/plugins && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.5.1 && rbenv global 2.5.1
+install openssl
+install libssl-dev
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc echo 'eval "$(rbenv init -)"' >> ~/.bashrc && mkdir -p ~/.rbenv/plugins && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.7.2 && rbenv global 2.7.2
 
 # Tmux
+install libevent-dev
+install libncurses5-dev
 wget https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b.tar.gz && tar -xzf tmux-3.1b.tar.gz && cd tmux-3.1b && ./configure && make && sudo make install && cd .. && rm -rf tmux-3.1*
 
-
 # Nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.36.0/install.sh | bash && nvm install node && npm install -g diff-so-fancy
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash && nvm install node && npm install -g diff-so-fancy
 
 # Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -136,5 +153,16 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Remove Nano
 remove nano
 
-# Topicons
-# google-chrome https://extensions.gnome.org/extension/495/topicons/
+echo "Manual steps:"
+echo "Install ExpressVPN (download directly from website)"
+echo "Install patched hid_apple (https://github.com/free5lot/hid-apple-patched)"
+echo "Install Franz (check franz-debian-bullseye)"
+echo "Check if tmux is being used in Guake"
+echo "Install fzf: git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf and run install"
+echo "Finish installing snap after logoff or reboot with sudo snap install core"
+echo "Install Heroku CLI sudo snap install --classic heroku"
+echo "Install Thunderbird sudo snap install thunderbird"
+echo "Install flameshot sudo snap install flameshot"
+echo "Add keyboard shortcut for Guake, Flameshot, 1Password"
+echo "Set keyboard as English (inlt. with AltGr dead keys)"
+echo "Install zeus pre: gem install --pre zeus"
