212ab780ced307951d39de703ed3908b3d82beda
Pedro Cavalheiro <pedrohc@gmail.com>
2023-04-21 18:24:16 +0200

    Format keymaps file and update gt binding

--- lua/pedro/keymaps.lua
+++ lua/pedro/keymaps.lua
@@ -10,16 +10,15 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
 -- Unmap some unwanted keys
-keymap("", "q:", "<Nop>", opts)   -- Disable : cmds history window
---keymap("", "Q", "<Nop>", opts)    -- Not sure, need to confirm
+keymap("", "q:", "<Nop>", opts) -- Disable : cmds history window
 
 -- Disable standardwindow navigation (to get used with new keymaps)
-keymap("", "<C-w>h", "<Nop>", opts)   
-keymap("", "<C-w>j", "<Nop>", opts)   
-keymap("", "<C-w>k", "<Nop>", opts)   
-keymap("", "<C-w>l", "<Nop>", opts)   
+keymap("", "<C-w>h", "<Nop>", opts)
+keymap("", "<C-w>j", "<Nop>", opts)
+keymap("", "<C-w>k", "<Nop>", opts)
+keymap("", "<C-w>l", "<Nop>", opts)
 
--- Aarrows and home keys 
+-- Arrows and home keys
 keymap("", "<Up>", "<NOP>", opts)
 keymap("", "<Down>", "<NOP>", opts)
 keymap("", "<Left>", "<NOP>", opts)
@@ -49,9 +48,9 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Fern navigation
-keymap("n", "<C-n>", ":Fern . -drawer -width=35 -toggle<CR><C-w>=", opts) -- Open fern
+keymap("n", "<C-n>", ":Fern . -drawer -width=35 -toggle<CR><C-w>=", opts)       -- Open fern
 keymap("n", "<Leader>f", ":Fern . -drawer -reveal=% -width=35<CR><C-w>=", opts) -- Open fern and reveal current file
-keymap("n", "<Leader>.", ":Fern %:h -drawer -width=35<CR><C-w>=", opts) -- Open fern with file path as root
+keymap("n", "<Leader>.", ":Fern %:h -drawer -width=35<CR><C-w>=", opts)         -- Open fern with file path as root
 
 -- Trouble navigation
 keymap("n", "<Leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", opts)
@@ -75,21 +74,21 @@ keymap("n", "<Leader>n", ":TestNearest<CR>", opts)
 keymap("n", "<Leader>l", ":TestLast<CR>", opts)
 keymap("n", "<Leader>g", ":TestVisit<CR>", opts)
 keymap("n", "<Leader>ss", "<ESC>:w<CR>:TestFile<CR>", opts)   -- Save and run tests for current file
-keymap("n", "<Leader>gt", ":A<CR> ", opts)                       -- Go to alternative file (test <> implementation)
+keymap("n", "gt", ":A<CR> ", opts)                            -- Go to alternative file (test <> implementation)
 keymap("n", "<leader>bp", "<ESC>Obinding.pry<ESC>,s,l", opts) -- Insert binding.pry and run nearest test
 
 -- Misc
-keymap("n", "<C-p>", ":Files<CR>", opts)                      -- Fuzzy find files
-keymap("n", "<C-g>", ":Rg<Cr>", opts)                          -- Fuzzy find in tags
-keymap("n", "<Leader>m", "@", opts)                           -- Quick access to macro
-keymap("n", "<Leader>rua", "<ESC>:RuboCop -a<CR>", opts)      -- Run Rubocop autocorrect
-keymap("n", "<Leader>ruaa", "<ESC>:RuboCop -A<CR>", opts)     -- Run Rubocop autocorrect with -A
-keymap("n", "<Leader>s", "<ESC>:w<CR>", opts)                 -- Save
-keymap("n", "<Leader>tt", "<ESC>:tabnew<CR>", opts)           -- New tab
-keymap("n", "<leader>q", "<ESC>:q<CR>", opts)                 -- Quit
-keymap("n", "<C-f>", "<ESC>:Format<CR>", opts)                -- Format
-keymap("n", "<CR>", ":nohlsearch<CR>", opts)                  -- Clear search highlight
-keymap("n", "<Leader>gb", "<ESC>:Git blame<CR>", opts)        -- Git blame
+keymap("n", "<C-p>", ":Files<CR>", opts)                  -- Fuzzy find files
+keymap("n", "<C-g>", ":Rg<Cr>", opts)                     -- Fuzzy find in tags
+keymap("n", "<Leader>m", "@", opts)                       -- Quick access to macro
+keymap("n", "<Leader>rua", "<ESC>:RuboCop -a<CR>", opts)  -- Run Rubocop autocorrect
+keymap("n", "<Leader>ruaa", "<ESC>:RuboCop -A<CR>", opts) -- Run Rubocop autocorrect with -A
+keymap("n", "<Leader>s", "<ESC>:w<CR>", opts)             -- Save
+keymap("n", "<Leader>tt", "<ESC>:tabnew<CR>", opts)       -- New tab
+keymap("n", "<leader>q", "<ESC>:q<CR>", opts)             -- Quit
+keymap("n", "<C-f>", "<ESC>:Format<CR>", opts)            -- Format
+keymap("n", "<CR>", ":nohlsearch<CR>", opts)              -- Clear search highlight
+keymap("n", "<Leader>gb", "<ESC>:Git blame<CR>", opts)    -- Git blame
 -- Clear trailing spaces with F6
 keymap("n", "<F6>", ":let _s=@/<Bar>:%s/\\s\\+$//e<Bar>:let @/=_s<Bar><CR>", opts)
 
