73e349ff8cd26dead07dec379a8b717dd7490837
Pedro Cavalheiro <pedrohc@gmail.com>
2025-06-16 20:42:54 +0200

    Replace Packer with Lazy

--- lua/pedro/plugins.lua
+++ lua/pedro/plugins.lua
@@ -1,120 +1,145 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
   return
 end
 
-return packer.startup(function(use)
+-- Setup lazy.nvim
+lazy.setup({
   -- SYNTAX HIGHLIGHTING
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'HiPhish/rainbow-delimiters.nvim'
-  use "slim-template/vim-slim"
-  use "lukas-reineke/indent-blankline.nvim" -- Indentation guides
+  { 'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = { "BufReadPost", "BufNewFile" },
+  },
+  { 'HiPhish/rainbow-delimiters.nvim', event = "BufReadPost" },
+  { "slim-template/vim-slim", ft = "slim" },
+  { "lukas-reineke/indent-blankline.nvim", event = "BufReadPost" }, -- Indentation guides
 
   -- EDITING & MOVEMENTS
-  use "mattesgroeger/vim-bookmarks" -- Vim Bookmarks upgrade
-  use "tpope/vim-repeat"            -- Remap . to repeat plugin remaps
-  use "unblevable/quick-scope"      -- Highlights letters for <f>
-  use "karb94/neoscroll.nvim"       -- Smooth scrolling
+  { "mattesgroeger/vim-bookmarks", keys = { "mm", "mi", "mn", "mp", "mc", "mx", "mkk", "mjj" } }, -- Vim Bookmarks upgrade
+  { "tpope/vim-repeat", event = "VeryLazy" },            -- Remap . to repeat plugin remaps
+  { "unblevable/quick-scope", event = { "BufReadPost", "BufNewFile" } },      -- Highlights letters for <f>
+  { "karb94/neoscroll.nvim", event = "VeryLazy" },       -- Smooth scrolling
 
   -- SEARCH
-  use "google/vim-searchindex" -- Adds info to search results (char /)
-  use {
+  { "google/vim-searchindex", event = "VeryLazy" }, -- Adds info to search results (char /)
+  {
     'junegunn/fzf.vim',
-    requires = { 'junegunn/fzf', run = ':call fzf#install()' }
-  }
+    dependencies = {
+      { 'junegunn/fzf', build = ':call fzf#install()' }
+    },
+    cmd = { "Files", "Buffers", "Rg", "Lines", "BLines", "Tags", "BTags", "Marks", "Windows", "History" },
+  },
 
   -- CODING
-  use "AndrewRadev/splitjoin.vim" -- Split/Join lines
-  use "vim-test/vim-test"          -- A Vim wrapper for running tests on different granularities.
-  use "scrooloose/nerdcommenter"  -- NERD commenter
-  use "tpope/vim-speeddating"     -- Adds date support to C-A and C-X
-  use "tpope/vim-surround"        -- Quotes substitution
-  use "tpope/vim-unimpaired"      -- Pairs of handy bracket mappings
+  { "AndrewRadev/splitjoin.vim", keys = { "gS", "gJ" } }, -- Split/Join lines
+  { "vim-test/vim-test", event = "VeryLazy" }, -- A Vim wrapper for running tests on different granularities.
+  { "scrooloose/nerdcommenter", keys = { "<leader>c", { "<leader>c", mode = "v" } } },  -- NERD commenter
+  { "tpope/vim-speeddating", keys = { "<C-a>", "<C-x>" } },     -- Adds date support to C-A and C-X
+  { "tpope/vim-surround", keys = { "cs", "ds", "ys" } },        -- Quotes substitution
+  { "tpope/vim-unimpaired", event = "VeryLazy" },      -- Pairs of handy bracket mappings
 
   -- CMP
-  use "hrsh7th/nvim-cmp"                                     -- The completion plugin
-  use { "L3MON4D3/LuaSnip" }                                 -- Snippet engine
-  use { "hrsh7th/cmp-buffer", after = "nvim-cmp" }           -- Buffer completions
-  use { "hrsh7th/cmp-path", after = "nvim-cmp" }             -- Path completions
-  use { "hrsh7th/cmp-cmdline", after = "nvim-cmp" }          -- Cmdline completions
-  use { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" }     -- Snippet completions
-  use { "rafamadriz/friendly-snippets", after = "nvim-cmp" } -- A bunch of snippets to use
-  use { "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" }         -- LSP source for CMP
-  use { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" }         -- Lua API source for CMP
-  use { "zbirenbaum/copilot.lua", after = "nvim-cmp",        -- Pure Lua replacement for Copilot
-    cmd = "Copilot", event = "InsertEnter", config = function() require("copilot").setup({}) end }
-  use { "zbirenbaum/copilot-cmp",                            -- Copilot source for CMP
-    after = { "copilot.lua" }, config = function() require("copilot_cmp").setup() end }
+  { "hrsh7th/nvim-cmp",                                      -- The completion plugin
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",           -- Buffer completions
+      "hrsh7th/cmp-path",             -- Path completions
+      "hrsh7th/cmp-cmdline",          -- Cmdline completions
+      "saadparwaiz1/cmp_luasnip",     -- Snippet completions
+      "hrsh7th/cmp-nvim-lsp",         -- LSP source for CMP
+      "hrsh7th/cmp-nvim-lua",         -- Lua API source for CMP
+      "L3MON4D3/LuaSnip",             -- Snippet engine
+      "rafamadriz/friendly-snippets", -- A bunch of snippets to use
+    }
+  },
+
+  -- Copilot
+  { "zbirenbaum/copilot.lua",                            -- Pure Lua replacement for Copilot
+    cmd = "Copilot",
+    event = "InsertEnter"
+  },
+  { "zbirenbaum/copilot-cmp",                            -- Copilot source for CMP
+    event = "InsertEnter",
+    dependencies = { "copilot.lua" }
+  },
 
   -- LSP
-  use "neovim/nvim-lspconfig"             -- Enable LSP
-  use "williamboman/mason.nvim"           -- Simple to use language server installer
-  use "williamboman/mason-lspconfig.nvim" -- Bridges mason.nvim with the lspconfig plugin
-  use 'nvimtools/none-ls.nvim'            -- LSP diagnostics and code actions (old null_ls)
+  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" } },             -- Enable LSP
+  { "williamboman/mason.nvim", cmd = "Mason", event = { "BufReadPre", "BufNewFile" } },           -- Simple to use language server installer
+  { "williamboman/mason-lspconfig.nvim", event = { "BufReadPre", "BufNewFile" } }, -- Bridges mason.nvim with the lspconfig plugin
+  { 'nvimtools/none-ls.nvim', event = { "BufReadPre", "BufNewFile" } },            -- LSP diagnostics and code actions (old null_ls)
 
   -- LANGUAGE SPECIFIC PLUGINS
-  use({ "elixir-tools/elixir-tools.nvim", tag = "stable", requires = { "nvim-lua/plenary.nvim" }})
-
-  -- AI
+  { "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    ft = { "elixir", "eex", "heex" },
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
 
   -- NAVIGATION & FILE CONFIGURATION
-  use { "nvim-tree/nvim-tree.lua",   -- Tree viewer
-    requires = "antoinemadec/FixCursorHold.nvim" }
-  use "tpope/vim-projectionist"      -- Configurable projections (like go to test file and back)
-  use "nvim-lualine/lualine.nvim"    -- Neovim statusline plugin written in lua
+  { "nvim-tree/nvim-tree.lua",   -- Tree viewer
+    event = "VeryLazy",
+    dependencies = { "antoinemadec/FixCursorHold.nvim" }
+  },
+  { "tpope/vim-projectionist", event = "VeryLazy" },      -- Configurable projections (like go to test file and back)
+  { "nvim-lualine/lualine.nvim", event = "VeryLazy" },    -- Neovim statusline plugin written in lua
 
   -- GIT
-  use "lewis6991/gitsigns.nvim" -- Git diff info in the sign column
-  use "tpope/vim-fugitive"      -- Git wrapper
+  { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" } }, -- Git diff info in the sign column
+  { "tpope/vim-fugitive", cmd = { "Git", "Gvdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit" } }, -- Git wrapper
 
   -- LINTERS/FORMATTERS
-  use "ngmy/vim-rubocop"           -- RuboCop
-  --use "prettier/vim-prettier"      -- Prettier
-  use 'mhartington/formatter.nvim' -- Formatter
+  { "ngmy/vim-rubocop", ft = "ruby" },           -- RuboCop
+  { 'mhartington/formatter.nvim', cmd = { "Format", "FormatWrite" } }, -- Formatter
 
   -- THEMES
-  use "folke/tokyonight.nvim"
-  use { "catppuccin/nvim", as = "catppuccin" }
+  { "folke/tokyonight.nvim", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- SYSTEM
-  --use "dhruvasagar/vim-prosession"
-  --use "tpope/vim-obsession"
-  use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
-  use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use { "folke/trouble.nvim",  -- A pretty list of diagnostics
-    requires = "nvim-tree/nvim-web-devicons" }
-  use "wincent/terminus"
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+  { "nvim-lua/plenary.nvim", lazy = true },  -- Useful lua functions used ny lots of plugins
+  { "nvim-lua/popup.nvim", lazy = true },    -- An implementation of the Popup API from vim in Neovim
+  { "folke/trouble.nvim",  -- A pretty list of diagnostics
+    cmd = { "TroubleToggle", "Trouble" },
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+  },
+  { "wincent/terminus", event = "VeryLazy" },
+}, {
+  -- Lazy.nvim configuration options
+  install = {
+    -- install missing plugins on startup
+    missing = true,
+  },
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
